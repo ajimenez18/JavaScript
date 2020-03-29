9067a34ab8cb8e15e7b6010839cee20c6f71cfe6
ajimenez18 <60615792+ajimenez18@users.noreply.github.com>
2020-03-29 16:42:59 +0200

    Add files via upload

---
+++ Act04/js/act04.js
@@ -0,0 +1,27 @@
+document.write("<br/>");
+var primera = 10;
+var segona = 20;
+var tercera = 30;
+var quarta = 40;
+
+var menor = Math.min(primera, segona, tercera, quarta);
+document.write("Tasca 1: El mes petit es -->");
+document.write(menor);
+document.write("<br/><br/><br/>");
+var a = 30;
+var b = 20;
+var c = 10;
+var d = 40;
+
+var major = Math.max(a, b, c, d);
+document.write("Tasca 2: El mes gran es -->");
+document.write(major);
+document.write("<br/><br/><br/>");
+var una = 10;
+var dos = 20;
+var tres = 30;
+var cuatro = 40;
+
+if (una !== dos && dos !== tres && tres !== cuatro) {
+  document.write(" Tasca 3 --> No son iguals");
+}
